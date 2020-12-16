390ebfecf4b774a061717a5770eb4bacb2fff796
CodenameRevy <codenamerevy@gmail.com>
2020-12-16 20:36:26 +0100

    bump

--- Seminarski/Source.cpp
+++ Seminarski/Source.cpp
@@ -7,37 +7,56 @@
 #define MAX 80
 #define BROJ 50;
 
+using namespace std;
+
 struct osoba
 {
-	int id_zaposlenog;
+	int idZaposlenog;
 	char* ime, * prezime;
 	char poruka[MAX + 1];
-	char* tel, * adresa_stanovanja;
+	char* tel, * adresaStanovanja;
 };
 
 void sifrovanje();
 void desifrovanje();
 
 
-void cezar(std::fstream f, int odstup);
-void vizner(std::fstream f, int odstup, char * sifra);
-void hill(std::fstream f, int broj);
+void cezar(fstream f, int odstup);
+void vizner(fstream f, int odstup, char * sifra);
+void hill(fstream f, int broj);
 
+void upis(osoba o, fstream f);
 void stampa(osoba o);
 
 int main()
 {
-	char c;
-	int n;
+	char c, imeBaze[MAX + 1];
+	int i, n;
 	osoba* baza;
-	do{
-	std::cout << "Do koliko clanova ce sadrzati baza?\nNAPOMENA: baza moze da sadrzi do 50 clanova: ";
-	std::cin >> n;
-	} while (n < 0 || n > 50);
 
+	cout << "Unesite ime baze: ";
+	cin >> imeBaze;
 
-	std::cout << "Unos podataka je uspesno zavrsen. Da li zelite da sifrujete unete podatke (D/N): ";
-	std::cin >> c;
+	//Upit za korisnika da unese broj osoba
+	do
+	{
+	cout << "Do koliko clanova ce sadrzati baza?\nNAPOMENA: baza moze da sadrzi do 50 clanova: ";
+	cin >> n;
+	if (n > 50)
+		cout << "Uneli ste prevelik broj!";
+	else if (n < 1)
+		cout << "Uneli ste premali broj!";
+	} 
+	while (n < 0 || n > 50);
+
+	//Pocetak unosenja podataka
+	for (i = 0; i < n; i++)
+	{
+
+	}
+
+	cout << "Unos podataka je uspesno zavrsen. Da li zelite da sifrujete unete podatke (D/N): ";
+	cin >> c;
 
 	switch (c)
 	{
@@ -60,6 +79,11 @@ void desifrovanje()
 
 }
 
+void upis(osoba o, fstream f)
+{
+
+}
+
 void stampa(osoba o)
 {
 
@@ -67,27 +91,16 @@ void stampa(osoba o)
 
 /*
 * Cezaova šifra je vrsta šifrovanja prostog teksta. Tekst se šifruje tako što se svako slovo teksta menja
-* odgovarajućim znakom koji je pomeren za odgovarajući broj mesta u levo. Na primer uzmemo pomeraj od 3 mesta, 
-* tad slovo D postaje slovo A, E postaje slovo B itd. Slova A, B i C postaju X, Y i Z. Ako primenimo ovo šifrovanje
-* na ASCII tabeli, imamo na raspolaganju 128 znakova, u poređenju sa 26 iz abecede.
+* odgovarajućim znakom koji je pomeren za odgovarajući broj mesta u levo
 */
-void cezar(std::fstream f, int odstup)
+void cezar(fstream f, int odstup)
 {
-
 }
 
 /*
 * Vižnerova šifra je modifikacija Cezarove šifre. Ovde se koristi ključna reč kao maska.
-* Poruka se šifruje uz pomoć te maske na sledeći način:
-* uzmeno rečenicu 'Almost before we knew it, we had left the ground' i ključnu reč 'MACHINE':
-* 
-* ALMOST BEFORE WE KNEW IT WE HAD LEFT THE GROUND
-* MACHIN EMACHI NE MACH IN EM ACH INEM ACH INEMAC
-* 
-* Rezultujući tekst je sledeći:
-* MLOVAG FQFQYM JI WNGD QG AQ HCK TRJF TJL OESGNF
 */
-void vizner(std::fstream f, int odstup, char* sifra)
+void vizner(fstream f, int odstup, char* sifra)
 {
 
 }
@@ -95,7 +108,7 @@ void vizner(std::fstream f, int odstup, char* sifra)
 * Hilovo šifrovanje je zasnovano na linearnoj algebri. Svako slovo se promeni u svoj redni broj
 * i ubaci se u n*n matricu, i pomnoži se sa željenim brojem. Rezultujuća matrica je šifrovan podatak.
 */
-void hill(std::fstream f, int broj)
+void hill(fstream f, int broj)
 {
 
 }
